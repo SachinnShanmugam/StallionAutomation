9bda713be5816c402b92d54a507b2c89c5f3145d
Sachinn Shanmugam <sachinshanmugam07@gmail.com>
2026-08-31 23:31:48 +0530

    Fix replay_gazebo_flight.bat CLI arguments for Gazebo playback mode

--- scripts/replay_gazebo_flight.bat
+++ scripts/replay_gazebo_flight.bat
@@ -7,6 +7,6 @@ echo  Launching Native Gazebo 3D Flight Replay GUI...
 echo =========================================================================
 
 wsl -d Ubuntu-22.04 -u root bash -c "mkdir -p /home/runner/work/StallionAutomation && ln -sfn /mnt/c/Users/SACHIN/Stallion /home/runner/work/StallionAutomation/StallionAutomation && chmod -R 777 /home/runner"
-wsl -d Ubuntu-22.04 bash -c "export GZ_SIM_SYSTEM_PLUGIN_PATH=/home/drones/ardupilot_gazebo/build; export GZ_SIM_RESOURCE_PATH=/mnt/c/Users/SACHIN/Stallion/gazebo/models:/home/runner/work/StallionAutomation/StallionAutomation/gazebo/models; export MESA_GL_VERSION_OVERRIDE=4.5; gz sim -r /mnt/c/Users/SACHIN/Stallion/gazebo/worlds/stallion_runway.sdf --playback /mnt/c/Users/SACHIN/Stallion/logs/gazebo_recordings"
+wsl -d Ubuntu-22.04 bash -c "export GZ_SIM_SYSTEM_PLUGIN_PATH=/home/drones/ardupilot_gazebo/build; export GZ_SIM_RESOURCE_PATH=/mnt/c/Users/SACHIN/Stallion/gazebo/models:/home/runner/work/StallionAutomation/StallionAutomation/gazebo/models; export MESA_GL_VERSION_OVERRIDE=4.5; gz sim -r --playback /mnt/c/Users/SACHIN/Stallion/logs/gazebo_recordings"
 
 pause
